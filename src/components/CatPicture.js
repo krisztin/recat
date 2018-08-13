4295d7bceea188b705780bc6b183049629d4d876
krisztin <k.nagy@wellcome.ac.uk>
2018-08-13 15:51:57 +0100

    rendering out catpics...not really, just blank placeholders for now

--- src/components/CatPicture.js
+++ src/components/CatPicture.js
@@ -2,9 +2,11 @@ import React from 'react';
 
 class CatPicture extends React.Component {
   render() {
+  const { image, name } = this.props.data;
+  
     return (
       <figure className="cat-pic">
-
+        <img src={image} alt={name} />
       </figure>
     )
   }
