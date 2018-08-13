d1e089cc1c4ceb06f952a4bf3b57b7b0654549cd
krisztin <k.nagy@wellcome.ac.uk>
2018-08-13 11:53:10 +0100

    adding some test cats

---
+++ src/test-cats.js
@@ -0,0 +1,46 @@
+const cats = [
+  {
+    "name": "Meowskin",
+    "image": "http://via.placeholder.com/350x150",
+    "age" : 2,
+    "excerpt" : "professional mouse hounter",
+    "gender": "male"
+  },
+  {
+    "name": "Purrcy",
+    "image": "http://via.placeholder.com/350x150",
+    "age" : 5,
+    "excerpt" : "Purring engaged",
+    "gender": "female"
+  },
+  {
+    "name": "Catzilla",
+    "image": "http://via.placeholder.com/350x150",
+    "age" : 1,
+    "excerpt" : "Will trample over your life",
+    "gender": "male"
+  },
+  {
+    "name": "Catniss",
+    "image": "http://via.placeholder.com/350x150",
+    "age" : 7,
+    "excerpt" : "Your one true leader",
+    "gender": "female"
+  },
+  {
+    "name": "Fluffykins",
+    "image": "http://via.placeholder.com/350x150",
+    "age" : 2,
+    "excerpt" : "Fun, fun, fluff",
+    "gender": "male"
+  },
+  {
+    "name": "Cat",
+    "image": "http://via.placeholder.com/350x150",
+    "age" : 19,
+    "excerpt" : "I hate kittens",
+    "gender": "male"
+  },
+]
+
+export default cats;
