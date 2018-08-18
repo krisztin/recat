820d41c3f0faa5ea410d6bf5fda1bfa84e9b599c
krisztin <k.nagy@wellcome.ac.uk>
2018-08-18 11:28:39 +0100

    purging

--- src/components/Header.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Header = props => (
-  <header className="App-header">
-    <h1>{props.title} <span role="img" aria-hidden="true">&#x1F431;</span></h1>
-  </header>
-)
-
-export default Header;
