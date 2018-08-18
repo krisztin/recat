b2e6e7f64f9361e3c16dee1c6bcd2fc91c213194
krisztin <k.nagy@wellcome.ac.uk>
2018-08-18 11:30:32 +0100

    adding basic Match structure

---
+++ src/components/Match.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Match extends React.Component {
+  render() {
+    return (
+      <div className="tagline">
+      <h1>It's a Match</h1>
+      <p>So what's next?</p>
+      <a href="tel:5555555555">Call Us at 555 555 555</a>
+      </div>
+    )
+  }
+}
+
+export default Match;
