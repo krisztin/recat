443a8ee9cc4a9d9278bd06317ea9a17914b990d3
krisztin <k.nagy@wellcome.ac.uk>
2018-08-18 11:29:25 +0100

    adding CatNotFound

---
+++ src/components/CatNotFound.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class CatNotFound extends React.Component {
+  render() {
+    <p>404 - Maybe you were looking for me? [insert random cat]</p>
+  }
+}
+
+export default CatNotFound;
